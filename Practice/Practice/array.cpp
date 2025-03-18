2421fcd5c559e92ebeb58a59be57a1780c4c3b15
Kim Sang Yun <grinder0617@gmail.com>
2025-03-18 19:00:43 +0900

    25.03.18
    25.03.18

--- Practice/Practice/array.cpp
+++ Practice/Practice/array.cpp
@@ -177,47 +177,30 @@ vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2)
 }
 */
 
-bool compare(pair<int, float>& a, pair<int, float>& b)
-{
-	if (a.second == b.second) {
-		return a.first < b.first;
-	}
-
-	return a.second > b.second;
-}
-
 vector<int> solution(int N, vector<int> stages) {
 	vector<int> answer;
-	vector<float> challenger(N + 2, 0.0);
+	vector<float> stage(N + 2, 0.0);
 	vector<float> fail(N + 2, 0.0);
 
-	for (int i = 0; i < stages.size(); ++i) {
-		for (int j = 1; j <= stages[i]; j++) {
-			++challenger[j];
+	for (int i = 0; i < stage.size(); ++i) {
+		for (int j = 1; j < stage[i]; ++j) {
+
 		}
-
-		++fail[stages[i]];
-	}
-
-	vector<pair<int, float>> failRatio(N);
-
-	for (int i = 0; i < N; ++i) {
-		failRatio[i].first = i + 1;
-
-		if (challenger[i + 1] == 0) {
-			failRatio[i].second == 0;
-		}
-
-		else {
-			failRatio[i].second = fail[i + 1] / challenger[i + 1];
-		}
-	}
-
-	sort(failRatio.begin(), failRatio.end(), compare);
-
-	for (int i = 0; i < N; ++i) {
-		answer.push_back(failRatio[i].first);
 	}
 
 	return answer;
+}
+
+void print(vector<int> vec)
+{
+	copy(vec.begin(), vec.end(), std::ostream_iterator<int>(cout, " "));
+	cout << endl;
+}
+
+int main()
+{
+	print(solution(5, { 2, 1, 2, 6, 2, 4, 3, 3 })); // 3 4 2 1 5
+	print(solution(4, { 4, 4, 4, 4, 4 }));          // 4 1 2 3 
+
+	return 0;
 }
