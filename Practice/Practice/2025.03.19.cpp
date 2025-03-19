10736ddcd826da3533dc8b62086e692828d917a5
Kim Sang Yun <grinder0617@gmail.com>
2025-03-20 01:21:27 +0900

    2025.03.20
    2025.03.20

--- Practice/Practice/2025.03.19.cpp
+++ Practice/Practice/2025.03.19.cpp
@@ -73,6 +73,7 @@ int solution(string s)
 }
 */
 
+/*
 vector<int> solution(vector<int> prices) {
     vector<int> answer(prices.size());
     stack<int> st;
@@ -97,4 +98,51 @@ vector<int> solution(vector<int> prices) {
     }
 
     return answer;
+}
+*/
+
+int solution(vector<vector<int>> board, vector<int> moves) {
+    vector<stack<int>> lanes(board[0].size());
+
+    for (int i = board.size() - 1; i >= 0; --i)
+    {
+        for (int j = 0; j < board[0].size(); ++j)
+        {
+            if (board[i][j])
+            {
+                lanes[j].push(board[i][j]);
+            }
+        }
+    }
+
+    stack<int> bucket;
+    int answer = 0;
+
+    for (int m : moves)
+    {
+        if (lanes[m - 1].size())
+        {
+            int doll = lanes[m - 1].top();
+            lanes[m - 1].pop();
+
+            if (bucket.size() && bucket.top() == doll)
+            {
+                bucket.pop();
+                answer += 2;
+            }
+
+            else
+            {
+                bucket.push(doll);
+            }
+        }
+    }
+
+    return answer;
+}
+
+int main()
+{
+    cout << solution({ {0, 0, 0, 0, 0}, {0, 0, 1, 0, 3}, {0, 2, 5, 0, 1}, {4, 2, 4, 4, 2}, {3, 5, 1, 3, 1} }, { 1, 5, 3, 5, 1, 2, 1, 4 }) << endl;
+    return 0;
 }
